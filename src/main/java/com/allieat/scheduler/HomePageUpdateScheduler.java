aa696ef26420e2818055ee4b21a7d3f383521179
CW Huang <an79322@hotmail.com>
2025-05-06 13:04:46 +0800

    1.新增三支Controller的測試 2.新增長輪詢用的排程器介面並修改排程器注入方式 3.修改後臺首頁的服務實作

--- src/main/java/com/allieat/scheduler/HomePageUpdateScheduler.java
+++ src/main/java/com/allieat/scheduler/HomePageUpdateScheduler.java
@@ -1,7 +1,7 @@
 package com.allieat.scheduler;
 
 
-import com.allieat.service.impl.BackStageHomePageServiceImpl;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
